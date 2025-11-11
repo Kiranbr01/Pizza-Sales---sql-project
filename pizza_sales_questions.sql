136dd70a9d60ed2ddcf1aacafd83fd0a516c410e
Kiran <161588904+Kiranbr01@users.noreply.github.com>
2025-11-11 14:48:51 +0530

    Update pizza_sales_questions.sql

--- pizza_sales_questions.sql
+++ pizza_sales_questions.sql
@@ -148,7 +148,3 @@ JOIN pizzas ON order_details.pizza_id = pizzas.pizza_id
 JOIN pizza_types ON pizzas.pizza_type_id = pizza_types.pizza_type_id
 GROUP BY pizza_types.category, pizzas.size
 ORDER BY pizza_types.category;
-
-
-
--- End of File
